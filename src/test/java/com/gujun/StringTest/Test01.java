157d8d33f872323bae28d3a9e21f591ee3bb872d
gujun <gujunchinawuxi@126.com>
2019-06-10 09:39:45 +0800

    2019.06.10

--- src/test/java/com/gujun/StringTest/Test01.java
+++ src/test/java/com/gujun/StringTest/Test01.java
@@ -22,6 +22,7 @@ public class Test01 {
         System.out.println(s1==s2);
         System.out.println(System.identityHashCode(s1));
         System.out.println(System.identityHashCode(s2));
+        System.out.println(123);
     }
 
 }
