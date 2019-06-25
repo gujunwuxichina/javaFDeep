2bbeaa9b34ad2225d519fe6c3cb9ee8105cab3df
gujun <gujunchinawuxi@126.com>
2019-06-26 06:55:47 +0800

    2019-06-26

---
+++ src/test/java/com/gujun/OO/AbstractClass.java
@@ -0,0 +1,17 @@
+package com.gujun.OO;
+
+/**
+ * @ClassName gu
+ * @Description TODO
+ * @Author GuJun
+ * @Date 2019/6/26 6:52
+ * @Version 1.0
+ **/
+public class AbstractClass {
+
+    //抽象类
+    //抽象类可以没有抽象方法，可以有构造器，但不能用来创建实例，而是被其子类调用；
+    //抽象类是从多个具有相同特征的类中抽象出来的一个抽象类，作为其子类的模板，避免子类设计的随意性；
+    //抽象类的普通方法可以依赖于其抽象类；
+
+}
