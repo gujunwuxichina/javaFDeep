07e851dd6e0645c5499aef2501ba5302ae44a824
gujun <gujunchinawuxi@126.com>
2019-06-26 21:14:14 +0800

    2019-06-26

---
+++ src/test/java/com/gujun/OO/innerClass/StaticInnerClass.java
@@ -0,0 +1,19 @@
+package com.gujun.OO.innerClass;
+
+/**
+ * @ClassName gu
+ * @Description TODO
+ * @Author GuJun
+ * @Date 2019/6/26 21:02
+ * @Version 1.0
+ **/
+public class StaticInnerClass {
+
+    //静态内部类
+    //使用static修饰，则该内部类不属于外部类的某个实例，而是属于外部类本身；
+    //静态内部类可以包含静态成员，也可以包含非静态成员；
+    //静态内部类只能访问外部类的类成员，不能访问非静态成员；
+    //静态内部类属于外部类的静态成员，所以外部类所有方法、代码块中可以使用静态内部类；
+    //
+
+}
