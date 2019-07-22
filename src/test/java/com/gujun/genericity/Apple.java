0d24d44c9a42a72fada2c7e39fae9d0a78a621a7
gujun <gujunchinawuxi@126.com>
2019-07-22 20:55:39 +0800

    2019-07-22

---
+++ src/test/java/com/gujun/genericity/Apple.java
@@ -0,0 +1,30 @@
+package com.gujun.genericity;
+
+/**
+ * @ClassName gu
+ * @Description TODO
+ * @Author GuJun
+ * @Date 2019/7/22 20:27
+ * @Version 1.0
+ **/
+public class Apple<T> {
+
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    //构造器还是原来的类名，不需要增加泛型声明；调用构造器时还是要带<>；
+
+    public Apple() {
+    }
+
+    public Apple(T t) {
+        this.t = t;
+    }
+}
---
+++ src/test/java/com/gujun/genericity/Test01.java
@@ -0,0 +1,43 @@
+package com.gujun.genericity;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName gu
+ * @Description TODO
+ * @Author GuJun
+ * @Date 2019/7/22 20:16
+ * @Version 1.0
+ **/
+public class Test01 {
+
+    //深入泛型：
+    //1.定义泛型接口、类：可以为任何类、接口增加泛型声明，并不是只有集合才有；
+    @Test
+    public void test01(){
+        Apple<String> apple1=new Apple<>("apple");
+        System.out.println(apple1.getT());
+        Apple<Integer> apple2=new Apple<>(22);
+        System.out.println(apple2.getT());
+    }
+    //2.从泛型类派生子类：
+    //当创建了带泛型声明的接口、父类之后，为接口创建实现类或从父类派生子类时，
+    //使用接口、父类时不能再包含泛型声明；如：
+    //A extends O<T>{...}是错误的；
+    //因为使用接口、父类时就必须为类型形参传入实际的类型；如：
+    //A extends O<String>{...}是正确的；也可以不传入实现类型（此时虚拟机会把类型形参当成Object）；
+
+    //并不存在泛型类
+    @Test
+    public void test02(){
+        List<String> list1=new ArrayList<>();
+        List<Integer> list2=new ArrayList<>();
+        System.out.println(list1.getClass()==list2.getClass()); //true
+        //依然被当成同一个类，在内存中占用同一块内存；
+        //因此在静态方法、静态初始化块或静态成员成员和初始化中不能使用类型形参；
+    }
+
+}
