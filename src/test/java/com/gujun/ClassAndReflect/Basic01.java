74c10a409de07a1a729e52c8e0215961f7da5f4d
gujun <gujunchinawuxi@126.com>
2019-08-04 23:27:10 +0800

    2019-08-04

---
+++ src/test/java/com/gujun/ClassAndReflect/Basic01.java
@@ -0,0 +1,17 @@
+package com.gujun.ClassAndReflect;
+
+/**
+ * @ClassName gu
+ * @Description TODO
+ * @Author GuJun
+ * @Date 2019/8/4 22:30
+ * @Version 1.0
+ **/
+public class Basic01 {
+
+    //系统可能在第一次使用某个类时加载该类，也可能采用预加载机制来加载某个类；
+    //类的加载：
+        //当程序主动使用某个类时，若该类还未被加载到内存中，则系统会通过加载，连接，初始化三个步骤对该类
+        //进行初始化；这三个步骤统称为类加载或类初始化；
+
+}
